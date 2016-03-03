bfc06840ad0de1eb25228873cb0efbe86ad52ef6
Todd Nestor <todd.nestor@gmail.com>
2016-03-03 15:59:33 -0800

    allows options now I hope

--- src/linkItUp.js
+++ src/linkItUp.js
@@ -6,15 +6,15 @@ else
         $.fn.linkItUp = function(options) {
             options = options || {};
 
-            var linkify = function( string ) {
+            var linkify = function( string, options ) {
                 var re = /(?:^|\ )(((?:(?:http(?:s)?\:)?(?:\/\/))|(?:\/\/))?(?:(?=[a-z0-9\-\.]{1,255}(?=\/|\ |$|\:|\?|\,|\!))(?:(?:(?:[a-z0-9]{1}(?:[a-z0-9\-]{1,62})?\.){1,127})[a-z]{2,}(?:\.[a-z]{2})?))(?:[a-z0-9\/\-\_\%\?\&\!\$\'\,\(\)\*\+\=\;])*?)(?=$|\.(?=\ |$)|\:|\ |\?(?=\ |$)|\,|\!)/ig;
 
                 return string.replace(re, function( match, p1, p2 ){
-                    return '<a ' + ( options && options.new_tab ? ' target="_blank" ' : '' ) + 'href="' + ( p2 ? '' : '//' ) + p1  + '">' + p1 + '</a>';
+                    return ( match.split('')[0] == ' ' ? ' ' : '' ) + '<a ' + ( options && options.new_tab ? ' target="_blank" ' : '' ) + 'href="' + ( p2 ? '' : '//' ) + p1  + '">' + p1 + '</a>';
                 });
-            }
+            };
 
-            this.html( linkify( this.html() ) );
+            this.html( linkify( this.html(), options ) );
 
             return this;
         };

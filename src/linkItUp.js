462925b8970f7150375a56ae3b7342d68171ba9a
Todd Nestor <todd.nestor@gmail.com>
2016-03-03 17:02:53 -0800

    updated now we have a 1.0 version

--- src/linkItUp.js
+++ src/linkItUp.js
@@ -7,10 +7,10 @@ else
             options = options || {};
 
             var linkify = function( string, options ) {
-                var re = /(?:^|\ )(((?:(?:http(?:s)?\:)?(?:\/\/))|(?:\/\/))?(?:(?=[a-z0-9\-\.]{1,255}(?=\/|\ |$|\:|\?|\,|\!))(?:(?:(?:[a-z0-9]{1}(?:[a-z0-9\-]{1,62})?\.){1,127})[a-z]{2,}(?:\.[a-z]{2})?))(?:[a-z0-9\/\-\_\%\?\&\!\$\'\,\(\)\*\+\=\;])*?)(?=$|\.(?=\ |$)|\:|\ |\?(?=\ |$)|\,|\!)/ig;
+                var re = /(?:^|\ |\n)(((?:(?:http(?:s)?\:)?(?:\/\/))|(?:\/\/))?(?:(?=[a-z0-9\-\.]{1,255}(?=\/|\ |$|\:|\?|\,|\!))(?:(?:(?:[a-z0-9]{1}(?:[a-z0-9\-]{1,62})?\.){1,127})[a-z]{2,}(?:\.[a-z]{2})?))(?:[a-z0-9\/\-\_\%\?\&\!\$\'\,\.\(\)\*\+\=\;])*?)(?=$|\.(?=\ |$)|\:|\n|\ |\?(?=\ |$)|\,|\!)/ig;
 
                 return string.replace(re, function( match, p1, p2 ){
-                    return ( match.split('')[0] == ' ' ? ' ' : '' ) + '<a ' + ( options && options.new_tab ? ' target="_blank" ' : '' ) + 'href="' + ( p2 ? '' : '//' ) + p1  + '">' + p1 + '</a>';
+                    return ( match.split('')[0] == ' ' ? ' ' : '' ) + '<a ' + ( options && options.new_tab ? ' target="_blank" ' : '' ) + ( options && options.style ? ' style="' + options.style + '" ' : '' ) + ( options && options.class ? ' class="' + options.class + '" ' : '' ) + ( options && options.title ? ' title="' + options.title + '" ' : '' ) + ( options && options.ref ? ' ref="' + options.ref + '" ' : '' ) + 'href="' + ( p2 ? '' : '//' ) + p1  + '">' + p1 + '</a>';
                 });
             };
 
